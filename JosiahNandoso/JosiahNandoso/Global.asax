e8e4f0790d0e09a3a14cf7b576780abf31ce2219
Josiah Martinez <josiahg2x@gmail.com>
2015-11-26 18:50:49 +1300

    Restart project, done controller

---
+++ JosiahNandoso/JosiahNandoso/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JosiahNandoso.WebApiApplication" Language="C#" %>
---
+++ JosiahNandoso/JosiahNandoso/Models/Specials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace JosiahNandoso.Models
+{
+    public class Specials
+    {
+        public int ID { get; set; }
+        public string SpecialName { get; set; }
+        public string SpecialPrice { get; set; }
+        public string SpecialDescription { get; set; }
+    }
+}
